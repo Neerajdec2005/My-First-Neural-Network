0aa70cf491e643c07cac271e6b7ad874e9d18915
Neerajdec2005 <neeraj.coder04@gmail.com>
2026-02-08 15:43:56 +0530

    adding another layer with 3x3 mat

--- neural_network.py
+++ neural_network.py
@@ -1,13 +1,24 @@
 import numpy as np
 
-_in= [1.3, 3.2, 4.5, 7.2]
+_in= [[1.3, 3.2, 4.5, 7.2],[-2.1, 4.3, 5.1, 6.8],[3.2, -5.1, 6.2, -2.1]]
 
-weights=[[1.2 ,5.7, 3.1, 2.4],
-         [1.7 ,4.5, 3.7, 1.9],
-         [4.1 ,2.2, 5.1, 2.9]]
+weights1=[[1.2 ,-5.7, 3.1, 2.4],
+         [-1.7 ,4.5, -3.7, 1.9],
+         [4.1 ,2.2, -5.1, -2.9]]
 
-bias=[4, 2.4, 3.4]
+bias1=[4, 2.4, 3.4]
 
-layer_outputs=np.dot(weights, _in) + bias
+# batch size of 32 is pretty common.
 
-print(layer_outputs)
+weights2=[[-1.2 ,5.7, 3.1],
+         [1.7 ,-4.5, 3.7],
+         [4.1 ,-2.2, 5.1]]
+
+bias2=[1.4, 2.1, 1]
+
+
+layer1=np.dot(_in, np.transpose(weights1)) + bias1
+
+layer2=np.dot(layer1, np.transpose(weights2)) + bias2
+
+print(layer2)
