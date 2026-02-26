8400b98ff0b5ca12aa5c7459f0375c9a78de4f5c
Neerajdec2005 <neeraj.coder04@gmail.com>
2026-02-26 17:31:58 +0530

    using learning rate decay

--- Neural_Network.py
+++ Neural_Network.py
@@ -90,22 +90,32 @@ class Activation_Softmax_Loss_CategoricalCrossentropy:
         self.dinputs= self.dinputs/samples
 
 class Optimizer_SGD:
-    def __init__(self, learning_rate=1.0):
+    def __init__(self, learning_rate=1.0, decay=0.0):
         self.learning_rate= learning_rate
+        self.current_learning_rate= learning_rate
+        self.decay= decay
+        self.iterations= 0
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate= self.learning_rate * (1.0/(1.0 + self.decay*self.iterations))
 
     def update_params(self, layer):
-        layer.weights+= -self.learning_rate * layer.dweights
-        layer.biases+= -self.learning_rate * layer.dbiases
+        layer.weights+= -self.current_learning_rate * layer.dweights
+        layer.biases+= -self.current_learning_rate * layer.dbiases
+
+    def post_update_params(self):
+        self.iterations+= 1
 
 
 X, y= spiral_data(samples=100, classes=3)
 
 # Forward Pass
-dense1= Layer_Dense(2,3)
+dense1= Layer_Dense(2,64)
 activation1= Activation_ReLU()
-dense2= Layer_Dense(3,3)
+dense2= Layer_Dense(64,3)
 loss_activation= Activation_Softmax_Loss_CategoricalCrossentropy()
-optimizers= Optimizer_SGD()
+optimizers= Optimizer_SGD(decay=1e-3)
 
 for epoch in range(10001):
     dense1.forward(X)
@@ -126,5 +136,7 @@ for epoch in range(10001):
     activation1.backward(dense2.dinputs)
     dense1.backward(activation1.dinputs)
 
+    optimizers.pre_update_params()
     optimizers.update_params(dense1)
     optimizers.update_params(dense2)
+    optimizers.post_update_params()
