99324e48a384cee5fbbfcd88926e65150937be7a
Neerajdec2005 <neeraj.coder04@gmail.com>
2026-02-26 17:18:12 +0530

    Simple neural network implementation

--- Neural_Network.py
+++ Neural_Network.py
@@ -89,6 +89,15 @@ class Activation_Softmax_Loss_CategoricalCrossentropy:
         self.dinputs[range(samples), y_true]-= 1
         self.dinputs= self.dinputs/samples
 
+class Optimizer_SGD:
+    def __init__(self, learning_rate=1.0):
+        self.learning_rate= learning_rate
+
+    def update_params(self, layer):
+        layer.weights+= -self.learning_rate * layer.dweights
+        layer.biases+= -self.learning_rate * layer.dbiases
+
+
 X, y= spiral_data(samples=100, classes=3)
 
 # Forward Pass
@@ -96,29 +105,26 @@ dense1= Layer_Dense(2,3)
 activation1= Activation_ReLU()
 dense2= Layer_Dense(3,3)
 loss_activation= Activation_Softmax_Loss_CategoricalCrossentropy()
+optimizers= Optimizer_SGD()
 
-dense1.forward(X)
-activation1.forward(dense1.output)
-dense2.forward(activation1.output)
-loss= loss_activation.forward(dense2.output, y)
+for epoch in range(10001):
+    dense1.forward(X)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    loss= loss_activation.forward(dense2.output, y)
 
-print("loss:", loss)
+    predictions= np.argmax(loss_activation.output, axis=1)
+    if len(y.shape)==2:
+        y= np.argmax(y, axis=1)
+    accuracy= np.mean(predictions==y)
 
-predictions= np.argmax(loss_activation.output, axis=1)
-if len(y.shape)==2:
-    y= np.argmax(y, axis=1)
-accuracy= np.mean(predictions==y)
+    if epoch%1000==0:
+        print(f"epoch: {epoch}, loss: {loss:.3f}, accuracy: {accuracy:.3f}")
+    # Backward pass
+    loss_activation.backward(loss_activation.output, y)
+    dense2.backward(loss_activation.dinputs)
+    activation1.backward(dense2.dinputs)
+    dense1.backward(activation1.dinputs)
 
-print("accuracy:", accuracy)
-
-# Backward pass
-loss_activation.backward(loss_activation.output, y)
-dense2.backward(loss_activation.dinputs)
-activation1.backward(dense2.dinputs)
-dense1.backward(activation1.dinputs)
-
-# Print gradients
-print (dense1.dweights)
-print (dense1.dbiases)
-print (dense2.dweights)
-print (dense2.dbiases)
+    optimizers.update_params(dense1)
+    optimizers.update_params(dense2)
